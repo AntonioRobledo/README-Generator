181bf2bcdaf0d9838dddc9fc953f4d9b89efdbb0
AntonioRobledo <robledoy@uci.edu>
2023-03-09 18:29:13 -0800

    added readme structure to markdown file

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdownFile = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/markdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter a name for your project: '
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a detailed description to help the user understand what your project is about: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps need to be taken in order to install necessities to your project?'
+    }, 
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe how to use your project: '
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'If necessary, describe how others can contribute to your project: '
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe what tests need to be run and how to run them: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Enter the license that your project contains: ',
+        choices:['Apache License 2.0', 'MIT License', 'GNU General Public License v3.0'],
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message:'Enter your GitHub username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email: '
+    }
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the current year in YYYY format: '
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a function to generate the README file
+function writeToFile(fileName, data) {
+    const readMe = generateMarkdown(data);
+    fs.writeFile('fileName', readMe, (err) => 
+    err ? console.log(err) : console.log('You have successfully created your README.md.')) 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+            .then(data) => {
+               let userLicense = data.license;
+               let licenseBadge = userLicense;
+        }
+}
 
 // Function call to initialize app
 init();
--- utils/markdown.js
+++ utils/markdown.js
@@ -1,18 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "None") {
+    return `[GitHub License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installations](#installation)
+
+- [Usage](#usage)
+
+- [Contributors](#credits)
+
+- [License](#license)
+
+## Installation
+
+Here is a guide to the installation process: 
+
+${data.installaion}
+
+## Usage 
+
+${data.usage}
+
+## Credits
+
+List any contributors here:
+
+## License 
+
+${data.license}
+
+## Badges 
+
+## Features 
+
+If your project has a lot of features, list them here: 
+
+## How to Contribute
+
+${data.contributions}
+
+## Tests
+
+Here's a list of tests and how to run them: 
+
+${data.tests}
 
 `;
 }
